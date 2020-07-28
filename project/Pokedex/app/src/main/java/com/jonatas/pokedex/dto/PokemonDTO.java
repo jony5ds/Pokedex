b2ddcfb594c8f4083ac6c2d7198945a8e21ff2f7
jony5ds <37637355+jony5ds@users.noreply.github.com>
2020-07-28 01:07:01 -0300

    Incluindo mais elementos na classe PokemonDTO

--- project/Pokedex/app/src/main/java/com/jonatas/pokedex/dto/PokemonDTO.java
+++ project/Pokedex/app/src/main/java/com/jonatas/pokedex/dto/PokemonDTO.java
@@ -1,8 +1,16 @@
 package com.jonatas.pokedex.dto;
 
+import java.util.List;
+
 public class PokemonDTO {
     public String nome;
-    public int codigo;
+    public long codigo;
+    public int hp;
+    public int ataque;
+    public int defesa;
+    public List<String> habilidades;
+    public List<String> tipos;
+    public List<String> evolucoes;
 
     public String getCodigo()
     {
