570f965e58c2a8536f844ab61493426160a1ccca
jony5ds <37637355+jony5ds@users.noreply.github.com>
2020-08-01 13:41:35 -0300

    Alterando classe pokemonDto

--- project/Pokedex/app/src/main/java/com/jonatas/pokedex/dto/PokemonDTO.java
+++ project/Pokedex/app/src/main/java/com/jonatas/pokedex/dto/PokemonDTO.java
@@ -1,23 +1,64 @@
 package com.jonatas.pokedex.dto;
 
+import com.jonatas.pokedex.model.Habilidade;
+import com.jonatas.pokedex.model.Sprite;
+import com.jonatas.pokedex.model.Tipo;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonDTO implements Serializable {
+    public int codigo;
     public String nome;
-    public String numero;
-    public long codigo;
     public int hp;
     public int ataque;
     public int defesa;
-    public List<String> habilidades;
-    public List<String> tipos;
-    public List<String> evolucoes;
+    public int velocidade;
+    public int ataqueEspecial;
+    public int defesaEspecial;
+    public List<Habilidade> habilidades;
+    public List<Tipo> tipos = new ArrayList<>();
+    public List<Sprite> sprites = new ArrayList<>();
+    public List<String> evolucoes = new ArrayList<>();
+
+    public PokemonDTO(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
 
     public String getCodigo()
     {
         return String.valueOf(codigo);
     }
+
+    public PokemonDTO create()
+    {
+        evolucoes = new ArrayList<>();
+        habilidades = new ArrayList<>();
+        tipos = new ArrayList<>();
+        this.ataque = 30;
+        this.defesa = 10;
+        this.evolucoes.add("Chaarlizard");
+        this.evolucoes.add("Chaminer");
+        this.hp = 100;
+        this.ataqueEspecial = 120;
+        this.defesaEspecial = 80;
+
+        Habilidade pular = new Habilidade("Pular",
+                "Esse pokemon consegue dar grandes saltos");
+        Habilidade socar = new Habilidade("Socar",
+                "Esse pokemon consegue dar socos");
+
+        habilidades.add(pular);
+        habilidades.add(socar);
+
+        Tipo tipo = new Tipo("Fogo");
+        tipos.add(tipo);
+
+        return this;
+    }
+
 
 
 }
